91231e9464e077cd99ff4323588b473e14eb7387
alaminkawsar <alaminkawsar8643@gmail.com>
2022-02-18 17:17:27 +0600

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Khayrul on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
